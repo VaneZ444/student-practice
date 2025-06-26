851a9f9c2f05fd4bcbb656d38f44356af2269456
Vladislav Danilov <Vladislav_Danilov@rntgroup.com>
2025-06-26 16:51:25 +0400

    Первоначальная настройка проекта.

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import {Flex, Layout} from 'antd';
+import {RouterProvider} from "react-router";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div></div>,
+    },
+]);
+
+function App() {
+  return (
+      <Flex>
+          <Layout>
+              <Sider width="25%">
+                  <Layout>
+                      <Header>CyberSportsPortal</Header>
+                  </Layout>
+              </Sider>
+              <Layout>
+                  <Content><RouterProvider router={router} /></Content>
+              </Layout>
+          </Layout>
+      </Flex>
+  );
+}
+
+export default App;
