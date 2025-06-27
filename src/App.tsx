2e107d3a95e6f8b439b43e689c4f24ae49e9daa5
Vladislav Danilov <Vladislav_Danilov@rntgroup.com>
2025-06-27 19:21:13 +0400

    Удалил лишние библиотеки.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
-import { AppLayout } from './components/AppLayout';
+import {Flex, Layout} from 'antd';
+import React, {useEffect} from 'react';
+import {RouterProvider} from "react-router";
+import {createBrowserRouter,} from "react-router-dom";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <AppLayout />
-    </BrowserRouter>
-  );
-};
+const {Header, Sider, Content} = Layout;
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div></div>,
+    },
+]);
+
+function App() {
+
+    const [state] = React.useState(0);
+
+    useEffect(() => {
+        console.log(state);
+    }, []);
+
+    return (
+        <Flex>
+            <Layout>
+                <Sider width="25%">
+                    <Layout>
+                        <Header>CyberSportsPortal</Header>
+                    </Layout>
+                </Sider>
+                <Layout>
+                    <Content><RouterProvider router={router}/></Content>
+                </Layout>
+            </Layout>
+        </Flex>
+    );
+}
 
 export default App;
